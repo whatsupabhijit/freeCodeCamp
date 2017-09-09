36d9d6eb0a6ab19e074ef4b156aad739d807118d
Abhijit <abhijit662000@gmail.com>
2017-09-09 14:30:48 +0000

    modified challenge5

--- learnyounodeSolutions/challenge5.js
+++ learnyounodeSolutions/challenge5.js
@@ -4,10 +4,11 @@ var dirname = process.argv[2];
 var ext = process.argv[3];
 var pat = RegExp('\\.' + ext + '$');
 
-file = fs.readdir(dirname, function(err, files) {
-  for (i = 0; i < files.length; i++) {
-    if (pat.test(files[i])) {
-      console.log(files[i]);
+fs.readdir(dirname, function(err, files){
+    if (!err){
+        for (var i = 0; i < files.length; i++){
+            if (pat.test(files[i]))
+                console.log(files[i]);
     }
   }
 });
